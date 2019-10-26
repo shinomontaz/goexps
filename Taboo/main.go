91c7e28de2ce8ec952ee1d8c7673d2a58ccac411
shinomontaz@gmail.com <shinomontaz@gmail.com>
2019-10-26 23:18:59 +0300

    taboo investigation

--- Taboo/main.go
+++ Taboo/main.go
@@ -45,13 +45,13 @@ func search(orders []*types.Order, tabooSize int, iterations int) *Result {
 	current.items = rand.Perm(len(orders))
 	current.edges = make([]Edge, 0, len(current.items)-1)
 	current.orders = orders
-	for idx, i := range current.items {
-		if idx == 0 {
-			continue
-		}
-		current.edges = append(current.edges, Edge{i - 1, i})
+	for idx := 1; idx < len(current.items); idx++ {
+		current.edges = append(current.edges, Edge{current.items[idx-1], current.items[idx]})
 	}
+
 	current.Fitness = current.Distance()
+
+	fmt.Println("start: ", current.items, current.Fitness)
 
 	best := current
 	tabuList := make([]Edge, 0, tabooSize)
@@ -67,8 +67,16 @@ func search(orders []*types.Order, tabooSize int, iterations int) *Result {
 			return candidates[i].Fitness < candidates[j].Fitness
 		})
 
+		for _, c := range candidates {
+			fmt.Println("candidate", c.items, c.Fitness)
+		}
+
+		panic("!")
+
 		if candidates[0].Fitness < best.Fitness {
 			best = candidates[0]
+
+			fmt.Println(i, ":", best.items, best.Fitness)
 
 			for _, edge := range best.edges {
 				tabuList = append(tabuList, edge)
@@ -91,10 +99,18 @@ func search(orders []*types.Order, tabooSize int, iterations int) *Result {
 
 func (r *Result) Clone() *Result {
 	clone := &Result{
-		items:   make([]int, len(r.items)),
+		items:   make([]int, 0, len(r.items)),
 		Fitness: r.Fitness,
-		edges:   make([]Edge, len(r.edges)),
+		edges:   make([]Edge, 0, len(r.edges)),
 		orders:  r.orders,
+	}
+
+	for _, it := range r.items {
+		clone.items = append(clone.items, it)
+	}
+
+	for _, e := range r.edges {
+		clone.edges = append(clone.edges, e)
 	}
 
 	return clone
@@ -141,13 +157,19 @@ func swap2opt(sol []int, i, j int) []int {
 func (r *Result) Mutate() *Result {
 	copy := r.Clone()
 
+	fmt.Println("before", copy.items)
+
 	i := rand.Intn(len(r.items) - 2)
 	j := i + rand.Intn(len(r.items)-i)
+
 	if i == j {
-		j++
+		j = i + rand.Intn(len(r.items)-i)
 	}
 
 	copy.items = swap2opt(r.items, i, j)
+	copy.Fitness = copy.Distance()
+
+	fmt.Println("after", copy.items, i, j)
 
 	return copy
 }
